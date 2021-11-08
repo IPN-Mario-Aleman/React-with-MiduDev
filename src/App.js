d5be49e8af1b667497ea3909354b5c479d99882d
Mario Aleman <mar.alemanf@gmail.com>
2021-11-07 18:55:48 -0600

    Creando la aplicacion con creat-react-app y agregando routes con wouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import getGifs from './services/getGifs';
-import Gif from './components/Gif'
+import ListOfGifs from './components/ListOfGifs'
+import {Link, Route} from 'wouter'
 
 function App() {
-  const [gifs, setGifs] = useState([])
-  useEffect(function (){
-    getGifs({ keyword:'programming' }).then(gifs => setGifs(gifs))
-  }, [])
+  const [keyword, setKeyword] = useState('german')
 
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(({title,id,url}) => 
-            <Gif 
-              key={id}
-              title={title} 
-              url={url} 
-              id={id}
-            />
-          )
-        }
+        <h1>App</h1>
+        <Link to="/gif/Anime">Gifs de Animes</Link>
+        <Link to="/gif/perros">Gifs de perros</Link>
+        <Link to="/gif/gatos">Gifs de gatos</Link>
+        <Link to="/gif/méxico">Gifs de México</Link>
+        <Route
+          component={ListOfGifs}
+          path="/gif/:keyword"
+        />
       </section>
     </div>
   );
