1b6fb24c2ab3a281b039effe322264f9e7ed172a
Mario Aleman <mar.alemanf@gmail.com>
2021-12-13 23:12:56 -0600

    Agregamos react memo para mejorar el rendimiento de la página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,22 @@
-import React, { useState} from "react";
+import React, { useCallback } from "react";
 import { useLocation} from "wouter";
 import ListOfGifs from "components/ListOfFGifs";
 import { useGifs } from "hooks/useGifs";
 import TrendingSearches from "components/TrendingSearches";
+import SearchForm from "components/SearchForm";
 
 export default function Home() {
-    const [keyword, setKeyword] = useState('');
     const [path, pushLocation] = useLocation();
     const { loading, gifs} = useGifs()
-    
-    const handleSubmit = evt =>{
-        evt.preventDefault()
+
+    const handleSubmit = useCallback(({keyword}) =>{
         // Navegar a otra ruta
         pushLocation(`/search/${keyword}`)
-    }
-    const handleChange = evt =>{
-        setKeyword(evt.target.value)
-    }
+    }, [pushLocation])
+
     return(
         <>
-            <form onSubmit={handleSubmit}>
-                <button>Buscar</button>
-                <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
-            </form>
+            <SearchForm onSubmit={handleSubmit}></SearchForm>
             <div className="App-main">
                 <div className="App-results">
                 <h3 className="App-title">Última búsqueda</h3>
